b28fa557037b9be11f44e57ea457882b6c16b969
Rizki Amalia Pratiwi <143069328+rizkiatiwi39@users.noreply.github.com>
2025-06-25 11:35:23 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace GUI_LM35_Ins_Elka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_sensor3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_sensor2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_sensor1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox_baudRate = new System.Windows.Forms.ComboBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.button_disconnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_comPort = new System.Windows.Forms.ComboBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Font = new System.Drawing.Font("Verdana", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(41, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(266, 29);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "\"Monitoring LM35\"";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBox2.Controls.Add(this.textBox_sensor3);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox_sensor2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox_sensor1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(1, 218);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(183, 134);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Data Receive";
+            // 
+            // textBox_sensor3
+            // 
+            this.textBox_sensor3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox_sensor3.Location = new System.Drawing.Point(88, 93);
+            this.textBox_sensor3.Name = "textBox_sensor3";
+            this.textBox_sensor3.ReadOnly = true;
+            this.textBox_sensor3.Size = new System.Drawing.Size(50, 24);
+            this.textBox_sensor3.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Teg  V";
+            // 
+            // textBox_sensor2
+            // 
+            this.textBox_sensor2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox_sensor2.Location = new System.Drawing.Point(88, 61);
+            this.textBox_sensor2.Name = "textBox_sensor2";
+            this.textBox_sensor2.ReadOnly = true;
+            this.textBox_sensor2.Size = new System.Drawing.Size(50, 24);
+            this.textBox_sensor2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nilai ADC";
+            // 
+            // textBox_sensor1
+            // 
+            this.textBox_sensor1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox_sensor1.Location = new System.Drawing.Point(88, 33);
+            this.textBox_sensor1.Name = "textBox_sensor1";
+            this.textBox_sensor1.ReadOnly = true;
+            this.textBox_sensor1.Size = new System.Drawing.Size(50, 24);
+            this.textBox_sensor1.TabIndex = 1;
+            this.textBox_sensor1.TextChanged += new System.EventHandler(this.textBox_sensor1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Suhu *C ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox_baudRate);
+            this.groupBox1.Controls.Add(this.button_connect);
+            this.groupBox1.Controls.Add(this.button_disconnect);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox_comPort);
+            this.groupBox1.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(1, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(346, 154);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial Port Setting";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(200, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 32);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "BaudRate :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBox_baudRate
+            // 
+            this.comboBox_baudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_baudRate.FormattingEnabled = true;
+            this.comboBox_baudRate.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBox_baudRate.Location = new System.Drawing.Point(102, 59);
+            this.comboBox_baudRate.Name = "comboBox_baudRate";
+            this.comboBox_baudRate.Size = new System.Drawing.Size(233, 25);
+            this.comboBox_baudRate.TabIndex = 3;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(102, 98);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(81, 33);
+            this.button_connect.TabIndex = 0;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // button_disconnect
+            // 
+            this.button_disconnect.Location = new System.Drawing.Point(243, 97);
+            this.button_disconnect.Name = "button_disconnect";
+            this.button_disconnect.Size = new System.Drawing.Size(92, 33);
+            this.button_disconnect.TabIndex = 4;
+            this.button_disconnect.Text = "Close";
+            this.button_disconnect.UseVisualStyleBackColor = true;
+            this.button_disconnect.Click += new System.EventHandler(this.button_disconnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port COM :";
+            // 
+            // comboBox_comPort
+            // 
+            this.comboBox_comPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_comPort.FormattingEnabled = true;
+            this.comboBox_comPort.Location = new System.Drawing.Point(102, 24);
+            this.comboBox_comPort.Name = "comboBox_comPort";
+            this.comboBox_comPort.Size = new System.Drawing.Size(233, 25);
+            this.comboBox_comPort.TabIndex = 1;
+            this.comboBox_comPort.DropDown += new System.EventHandler(this.comboBox_comPort_DropDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GUI_LM35_Ins_Elka.Properties.Resources.pngtree_cartoon_thermometer_icon_download_image_2292413;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(347, 364);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "019 Rizki Amalia Pratiwi";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_disconnect;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.ComboBox comboBox_baudRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox_comPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_sensor3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_sensor2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_sensor1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace GUI_LM35_Ins_Elka
+{
+    public partial class Form1 : Form
+    {
+        string serialDataIn;
+        sbyte indexOfA, indexOfB, indexOfC;
+        string dataSensor1, dataSensor2, dataSensor3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button_connect.Enabled = true;
+            button_disconnect.Enabled = false;
+            //verticalProgressBar_statusCom.Value = 0;
+            comboBox_baudRate.Text = "9600";
+        }
+
+        private void comboBox_comPort_DropDown(object sender, EventArgs e)
+        {
+            string[] portLists = SerialPort.GetPortNames();
+            comboBox_comPort.Items.Clear();
+            comboBox_comPort.Items.AddRange(portLists);
+
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox_comPort.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBox_baudRate.Text);
+                serialPort1.Open();
+
+                pictureBox1.BackColor = Color.Green;
+
+                button_connect.Enabled = false;
+                button_disconnect.Enabled = true;
+                
+                //verticalProgressBar_statusCom.Value = 100;
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_sensor1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_disconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+
+                button_connect.Enabled = true;
+                button_disconnect.Enabled = false;
+                //verticalProgressBar_statusCom.Value = 0;
+
+                pictureBox1.BackColor = Color.Red;
+                textBox_sensor1.Text = "0";
+                textBox_sensor2.Text = "0";
+                textBox_sensor3.Text = "0";
+
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            serialDataIn = serialPort1.ReadLine();
+            this.BeginInvoke(new EventHandler(ProcessData));
+        }
+
+        private void ProcessData(object sender, EventArgs e)
+        {
+            try
+            {
+                indexOfA = (sbyte)Convert.ToByte( serialDataIn.IndexOf("A"));
+                indexOfB = (sbyte)Convert.ToByte( serialDataIn.IndexOf("B"));
+                indexOfC = (sbyte)Convert.ToByte( serialDataIn.IndexOf("C"));
+
+                dataSensor1 = serialDataIn.Substring(0, indexOfA);
+                dataSensor2 = serialDataIn.Substring(indexOfA + 1, (indexOfB - indexOfA) - 1);
+                dataSensor3 = serialDataIn.Substring(indexOfB + 1, (indexOfC - indexOfB) - 1);
+
+                textBox_sensor1.Text = dataSensor1;
+                textBox_sensor2.Text = dataSensor2;
+                textBox_sensor3.Text = dataSensor3;
+
+                //verticalProgressBar2.Value = Convert.ToInt32(dataSensor1);
+                //verticalProgressBar3.Value = Convert.ToInt32(dataSensor2);
+                //verticalProgressBar4.Value = Convert.ToInt32(dataSensor3);
+
+                //progressBar1.Value = Convert.ToInt32(dataSensor1);
+                //progressBar2.Value = Convert.ToInt32(dataSensor2);
+                //progressBar3.Value = Convert.ToInt32(dataSensor3);
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
